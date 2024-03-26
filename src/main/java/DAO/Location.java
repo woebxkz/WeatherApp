972f0de6e4cec169b641277a8d5bd1dd3439c26f
weron <weronika.jozwiak19@gmail.com>
2024-03-26 17:09:08 +0100

    first commit

---
+++ src/main/java/DAO/Location.java
@@ -0,0 +1,99 @@
+package DAO;
+
+import java.util.UUID;
+import util.CreationException;
+
+public class Location {
+    private UUID id;
+    private double longtitude; // długość geograficzna
+    private double latitude; // szerokość geograficzna;
+    private String city;
+    private String region;
+    private String country;
+
+
+    public Location(UUID id,
+                    double longtitude,
+                    double latitude,
+                    String city,
+                    String region,
+                    String country) throws CreationException {
+        super();
+
+        if (id == null) id = UUID.randomUUID();
+        checkValues(id, longtitude, latitude, city, region, country);
+
+        this.id = id;
+        this.longtitude = longtitude;
+        this.latitude = latitude;
+        this.city = city;
+        this.region = region;
+        this.country = country;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+
+    public String getRegion() {
+        return region;
+    }
+
+
+    public void setRegion(String region) {
+        this.region = region;
+    }
+
+
+    public UUID getId() {
+        return id;
+    }
+
+
+    public double getLongtitude() {
+        return longtitude;
+    }
+
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+
+    public String getCountry() {
+        return country;
+    }
+
+
+    private void checkValues(UUID id, double longtitude, double latitude, String city, String region,
+                             String country) throws CreationException {
+
+        if (longtitude < -180 || longtitude > 180) cee("Nieprawdiłowa długość geograficzna");
+        if (latitude < -90 || latitude > 90) cee("Nieprawdiłowa szerokość geograficzna");
+        if (city == null || city.length() == 0) cee("Nazwa miasta nie może być pusta");
+        if (country == null || country.length() == 0) cee("Nazwa kraju nie może być pusta");
+    }
+
+
+    private void cee(String msg) throws CreationException {
+        throw new CreationException(msg);
+    }
+
+
+    @Override
+    public String toString() {
+        return "Location [id=" + id
+                + ", longtitude=" + longtitude
+                + ", latitude=" + latitude
+                + ", city=" + city
+                + ", region=" + region
+                + ", country=" + country + "]";
+    }
+}
+
---
+++ src/main/java/util/CreationException.java
@@ -0,0 +1,8 @@
+package util;
+
+public class CreationException extends Exception{
+
+    public CreationException(String msg) {
+        super(msg);
+    }
+}
